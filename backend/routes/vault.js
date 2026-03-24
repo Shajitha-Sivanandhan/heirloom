f7c5f50256596d4d36c8877d45fac158adb06efb
Shajitha <shajitha2527@gmail.com>
2026-03-25 00:37:15 +0530

    Removed old project files

--- backend/routes/vault.js
+++
@@ -1,9 +0,0 @@
-// routes/vault.js
-const express = require("express");
-const router = express.Router();
-const vaultController = require("../controllers/vaultController");
-
-// POST /api/vault
-router.post("/vault", vaultController.createVault);
-
-module.exports = router;
